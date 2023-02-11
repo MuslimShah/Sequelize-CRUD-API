71e20ff52e3b07d8e8fa3c98b01e39ebea6f491d
Muslim shah <smuslimshah7@gmail.com>
2023-02-11 14:30:15 +0500

    fetching single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,39 @@
 const User = require('../database/index').User;
 
+
+//GET ALL USERS 
 module.exports.getUsers = async(req, res, next) => {
-    const users = await User.findAll();
-    res.json(users);
+    try {
+        const users = await User.findAll();
+        if (!users)
+            return res.json({ error: `no users found` })
+
+        res.json({ msg: 'users found', users });
+
+
+    } catch (err) {
+        console.log(`error fetchig all users `);
+    }
+
 }
 
+//GET SINGLE USER
+module.exports.getUser = async(req, res, next) => {
+    const id = req.params.id;
+
+    try {
+        const user = await User.findByPk(id);
+        if (!user)
+            return res.json({ error: `user with id: ${id} not found` })
+
+        res.json({ msg: 'user found', user });
+
+    } catch (err) {
+        console.log(`error occured getting single user :${err}`);
+    }
+}
+
+// POST USER DATA 
 module.exports.postUser = async(req, res) => {
 
     const user = {
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const userController = require('../controllers/userController')
 const router = express.Router();
-
+//GET ==> ALL USERS
 router.get('/users', userController.getUsers);
+//GET ==> SINGLE USER
+router.get('/user/:id', userController.getUser);
+//POST ==>USER DATA
 router.post('/add', userController.postUser)
 module.exports = router;
